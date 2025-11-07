5ed7fbf52506a921883e7f85ce818f26c679c225
Gilles Chehade <gilles@poolp.org>
2025-11-07 11:56:55 +0100

    fix all test failures caused by cast mismatch, there are still two failures to understand

--- subcommands/check/stdio.go
+++ subcommands/check/stdio.go
@@ -1,6 +1,7 @@
 package check
 
 import (
+	"github.com/PlakarKorp/kloset/objects"
 	"github.com/PlakarKorp/plakar/appcontext"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -16,48 +17,48 @@ func eventsProcessorStdio(ctx *appcontext.AppContext, quiet bool) chan struct{} 
 		for event := range ctx.Events().Listen() {
 			switch event.Type {
 			case "snapshot.check.directory.missing":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
 				pathname := event.Data["path"].(string)
 				ctx.GetLogger().Warn("%x: %s %s: missing directory", snapshotID[:4], crossMark, pathname)
 			case "snapshot.check.file.missing":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
 				pathname := event.Data["path"].(string)
 				ctx.GetLogger().Warn("%x: %s %s: missing file", snapshotID[:4], crossMark, pathname)
 			case "snapshot.check.object.missing":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
-				contentMac := event.Data["content_mac"].([32]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
+				contentMac := event.Data["content_mac"].(objects.MAC)
 				ctx.GetLogger().Warn("%x: %s %x: missing object", snapshotID[:4], crossMark, contentMac)
 			case "snapshot.check.chunk.missing":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
-				contentMac := event.Data["content_mac"].([32]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
+				contentMac := event.Data["content_mac"].(objects.MAC)
 				ctx.GetLogger().Warn("%x: %s %x: missing chunk", snapshotID[:4], crossMark, contentMac)
 
 			case "snapshot.check.directory.corrupted":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
 				pathname := event.Data["path"].(string)
 				ctx.GetLogger().Warn("%x: %s %s: corrupted directory", snapshotID[:4], crossMark, pathname)
 			case "snapshot.check.file.corrupted":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
 				pathname := event.Data["path"].(string)
 				ctx.GetLogger().Warn("%x: %s %s: corrupted file", snapshotID[:4], crossMark, pathname)
 			case "snapshot.check.object.corrupted":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
-				contentMac := event.Data["content_mac"].([32]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
+				contentMac := event.Data["content_mac"].(objects.MAC)
 				ctx.GetLogger().Warn("%x: %s %x: corrupted object", snapshotID[:4], crossMark, contentMac)
 			case "snapshot.check.chunk.corrupted":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
-				contentMac := event.Data["content_mac"].([32]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
+				contentMac := event.Data["content_mac"].(objects.MAC)
 				ctx.GetLogger().Warn("%x: %s %x: corrupted chunk", snapshotID[:4], crossMark, contentMac)
 
 			case "snapshot.check.directory.ok", "snapshot.check.file.ok":
 				if !quiet {
-					snapshotID := event.Data["snapshot_id"].([16]byte)
+					snapshotID := event.Data["snapshot_id"].(objects.MAC)
 					pathname := event.Data["path"].(string)
 					ctx.GetLogger().Info("%x: %s %s", snapshotID[:4], checkMark, pathname)
 				}
 
 			case "snapshot.check.directory.error", "snapshot.check.file.error":
-				snapshotID := event.Data["snapshot_id"].([16]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
 				pathname := event.Data["path"].(string)
 				errorMessage := event.Data["error"].(string)
 				ctx.GetLogger().Stderr("%x: KO %s %s: %s", snapshotID[:4], crossMark, pathname, errorMessage)
--- subcommands/restore/stdio.go
+++ subcommands/restore/stdio.go
@@ -1,6 +1,7 @@
 package restore
 
 import (
+	"github.com/PlakarKorp/kloset/objects"
 	"github.com/PlakarKorp/plakar/appcontext"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -16,14 +17,14 @@ func eventsProcessorStdio(ctx *appcontext.AppContext, quiet bool) chan struct{} 
 		for event := range ctx.Events().Listen() {
 			switch event.Type {
 			case "snapshot.restore.path.error", "snapshot.restore.directory.error", "snapshot.restore.file.error":
-				snapshotID := event.Data["snapshot_id"].([]byte)
+				snapshotID := event.Data["snapshot_id"].(objects.MAC)
 				pathname := event.Data["path"].(string)
 				errorMessage := event.Data["error"].(string)
 				ctx.GetLogger().Warn("%x: KO %s %s: %s", snapshotID[:4], crossMark, pathname, errorMessage)
 
 			case "snapshot.restore.path.ok", "snapshot.restore.directory.ok", "snapshot.restore.file.ok":
 				if !quiet {
-					snapshotID := event.Data["snapshot_id"].([]byte)
+					snapshotID := event.Data["snapshot_id"].(objects.MAC)
 					pathname := event.Data["path"].(string)
 					ctx.GetLogger().Info("%x: OK %s %s", snapshotID[:4], checkMark, pathname)
 				}
