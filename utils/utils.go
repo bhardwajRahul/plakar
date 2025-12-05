9e170ee7e8a0c350fab14db31f387be9db8f55f4
Gilles Chehade <gilles@poolp.org>
2025-12-05 14:38:19 +0100

    bump

--- utils/utils.go
+++ utils/utils.go
@@ -363,7 +363,7 @@ func GetDataDir(appName string) (string, error) {
 }
 
 var VERSION = func() string {
-	version := "v1.0.6-devel"
+	version := "v1.1.0-devel"
 	if strings.HasSuffix(version, "-devel") {
 		if info, ok := debug.ReadBuildInfo(); ok {
 			for _, setting := range info.Settings {
