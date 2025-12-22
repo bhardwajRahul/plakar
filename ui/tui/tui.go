a688990b55894989106479274785e26cd98ad12c
Gilles Chehade <gilles@poolp.org>
2025-12-22 12:43:57 +0100

    fallback to stdio

--- ui/tui/tui.go
+++ ui/tui/tui.go
@@ -1,10 +1,9 @@
 package tui
 
 import (
-	"github.com/PlakarKorp/kloset/objects"
 	"github.com/PlakarKorp/plakar/appcontext"
+	"github.com/PlakarKorp/plakar/ui/stdio"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/dustin/go-humanize"
 )
 
 type commandApp struct {
@@ -27,127 +26,40 @@ func Run(ctx *appcontext.AppContext) func() {
 
 		var app *commandApp
 
-		for {
-			select {
-			case <-ctx.Done():
-				if app != nil {
+		for e := range events {
+
+			if app != nil {
+				app.events <- *e
+				if e.Type == "workflow.end" {
 					close(app.events)
 					<-app.done
 					app = nil
 				}
-				done <- ctx.Err()
-				return
+				continue
+			}
 
-			case e, ok := <-events:
-				if !ok {
-					if app != nil {
-						close(app.events)
-						<-app.done
-						app = nil
-					}
-
-					done <- nil
-					return
-				}
-
+			if e.Type == "workflow.start" {
+				app = startApp(ctx, e.Data["workflow"].(string))
 				if app != nil {
 					app.events <- *e
-					if e.Type == "workflow.end" {
-						close(app.events)
-						<-app.done
-						app = nil
-					}
 					continue
 				}
-
-				if e.Type == "workflow.start" {
-					app = startApp(ctx, e.Data["workflow"].(string))
-					if app != nil {
-						app.events <- *e
-						continue
-					}
-				}
-
-				if ctx.Silent {
-					continue
-				}
-
-				if ctx.Quiet && e.Level == "info" {
-					continue
-				}
-
-				switch e.Type {
-				case "path", "directory", "file", "symlink":
-					// ignore, displayed as either success or failure
-
-				case "path.error", "directory.error", "file.error", "symlink.error":
-					snapshotID := e.Snapshot
-					pathname := e.Data["path"].(string)
-					errorMessage := e.Data["error"].(error)
-					ctx.GetLogger().Stderr("TUI: %x: KO %s %s: %s", snapshotID[:4], crossMark, pathname, errorMessage)
-
-				case "path.ok", "directory.ok", "file.ok", "symlink.ok":
-					snapshotID := e.Snapshot
-					pathname := e.Data["path"].(string)
-					ctx.GetLogger().Stdout("TUI: %x: OK %s %s", snapshotID[:4], checkMark, pathname)
-
-				case "object", "chunk":
-					// ignore, too verbose for stdio
-
-				case "object.ok", "chunk.ok":
-					// ignore, too verbose for stdio
-
-				case "object.error", "chunk.error":
-					snapshotID := e.Snapshot
-					mac := e.Data["mac"].(objects.MAC)
-					errorMessage := e.Data["error"].(error)
-					ctx.GetLogger().Stderr("TUI: %x: KO %s object=%x: %s", snapshotID[:4], crossMark, mac, errorMessage)
-
-				case "snapshot.backup.result":
-					snapshotID := e.Snapshot
-					totalSize := humanize.IBytes(e.Data["size"].(uint64))
-					duration := e.Data["duration"]
-					rbytes := humanize.IBytes(uint64(e.Data["rbytes"].(int64)))
-					wbytes := humanize.IBytes(uint64(e.Data["wbytes"].(int64)))
-					errors := e.Data["errors"].(uint64)
-					ctx.GetLogger().Stdout("TUI: %x: created snapshot of logical size %s to %s in %s with %d errors (in: %s, out: %s)",
-						snapshotID[:4], totalSize, e.Data["target"].(string), duration, errors, rbytes, wbytes)
-
-				case "snapshot.restore.result":
-					snapshotID := e.Snapshot
-					totalSize := humanize.IBytes(e.Data["size"].(uint64))
-					duration := e.Data["duration"]
-					rbytes := humanize.IBytes(uint64(e.Data["rbytes"].(int64)))
-					wbytes := humanize.IBytes(uint64(e.Data["wbytes"].(int64)))
-					errors := e.Data["errors"].(uint64)
-					ctx.GetLogger().Stdout("TUI: %x: restored snapshot of logical size %s to %s in %s with %d errors (in: %s, out: %s)",
-						snapshotID[:4], totalSize, e.Data["target"].(string), duration, errors, rbytes, wbytes)
-
-				case "snapshot.check.result":
-					snapshotID := e.Snapshot
-					totalSize := humanize.IBytes(e.Data["size"].(uint64))
-					duration := e.Data["duration"]
-					rbytes := humanize.IBytes(uint64(e.Data["rbytes"].(int64)))
-					wbytes := humanize.IBytes(uint64(e.Data["wbytes"].(int64)))
-					errors := e.Data["errors"].(uint64)
-					success := "succeeded"
-					if errors != 0 {
-						success = "failed"
-					}
-					ctx.GetLogger().Stdout("TUI: %x: check %s snapshot of logical size %s in %s with %d errors (in: %s, out: %s)",
-						snapshotID[:4], success, totalSize, duration, errors, rbytes, wbytes)
-
-				default:
-					//fmt.Printf("%T: %s\n", e, e.Type)
-				}
-
 			}
+
+			stdio.HandleEvent(ctx, e)
 		}
+
+		if app != nil {
+			close(app.events)
+			<-app.done
+			app = nil
+		}
+
+		done <- nil
 	}()
 
 	// wait function, as before
 	return func() {
-		ctx.GetLogger().Warn("TUI: waiting for completion...")
 		<-done
 	}
 }
