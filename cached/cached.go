b72940ec12fbb2f56a09524fac56f5e961158f0c
Mathieu Masson <mathieu.masson@plakar.io>
2026-01-02 12:14:31 +0100

    cached: Add traces (client side)
    * Adds traces for both cached entry point mainly to allow debugging
      perf issues.

--- cached/cached.go
+++ cached/cached.go
@@ -43,6 +43,11 @@ var (
 )
 
 func RebuildStateFromStateFile(ctx *appcontext.AppContext, stateID objects.MAC, repoID uuid.UUID, storeConfig map[string]string) (int, error) {
+	t0 := time.Now()
+	defer func() {
+		ctx.GetLogger().Trace("cached", "rebuild from local statefile (file=%x, store=%s): %s", stateID, repoID, time.Since(t0))
+	}()
+
 	req := &RequestPkt{
 		Secret:      ctx.GetSecret(),
 		RepoID:      repoID,
@@ -54,6 +59,10 @@ func RebuildStateFromStateFile(ctx *appcontext.AppContext, stateID objects.MAC, 
 }
 
 func RebuildStateFromStore(ctx *appcontext.AppContext, repoID uuid.UUID, storeConfig map[string]string) (int, error) {
+	t0 := time.Now()
+	defer func() {
+		ctx.GetLogger().Trace("cached", "rebuild from store (store=%s): %s", repoID, time.Since(t0))
+	}()
 	req := &RequestPkt{
 		Secret:      ctx.GetSecret(),
 		RepoID:      repoID,
