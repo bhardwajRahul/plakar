07ec01c78a6b7c6478ce1d1cdfa40c00ae6f071f
Omar Polo <op@omarpolo.com>
2026-05-06 07:22:42 +0000

    token-create: minor nits

--- subcommands/login/token-create.go
+++ subcommands/login/token-create.go
@@ -22,7 +22,7 @@ import (
 
 	"github.com/PlakarKorp/kloset/repository"
 	"github.com/PlakarKorp/plakar/appcontext"
-	plogin "github.com/PlakarKorp/plakar/login"
+	"github.com/PlakarKorp/plakar/login"
 	"github.com/PlakarKorp/plakar/subcommands"
 )
 
@@ -35,7 +35,7 @@ func init() {
 }
 
 func (cmd *TokenCreate) Parse(ctx *appcontext.AppContext, args []string) error {
-	flags := flag.NewFlagSet("token-create", flag.ExitOnError)
+	flags := flag.NewFlagSet("token create", flag.ExitOnError)
 	flags.Usage = func() {
 		fmt.Fprintf(flags.Output(), "Usage: token create\n")
 	}
@@ -48,7 +48,7 @@ func (cmd *TokenCreate) Parse(ctx *appcontext.AppContext, args []string) error {
 }
 
 func (cmd *TokenCreate) Execute(ctx *appcontext.AppContext, repo *repository.Repository) (int, error) {
-	token, err := plogin.DeriveToken(ctx)
+	token, err := login.DeriveToken(ctx)
 	if err != nil {
 		return 1, err
 	}
