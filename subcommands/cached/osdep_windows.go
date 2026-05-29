5265007b7191ccc1bb6323ec1df262528162fb97
Omar Polo <op@omarpolo.com>
2026-05-29 13:30:56 +0000

    cached/windows: do not fail in setupSyslog
    we don't have syslog on windows, we'd need to better integrate with
    the systemm.  Having this being logged every time it's a pain, so let's
    disable it.

--- subcommands/cached/osdep_windows.go
+++ subcommands/cached/osdep_windows.go
@@ -2,12 +2,14 @@ package cached
 
 import (
 	"errors"
+	"io"
 
 	"github.com/PlakarKorp/plakar/appcontext"
 )
 
 func setupSyslog(ctx *appcontext.AppContext) error {
-	return errors.ErrUnsupported
+	ctx.GetLogger().SetOutput(io.Discard)
+	return nil
 }
 
 func daemonize(argv []string) error {
